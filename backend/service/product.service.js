ea67871a2bff3c428b882e5b07c765bce0cadcdb
JoseParedesC <kbayro200@gmail.com>
2025-10-28 22:59:44 -0500

    change products backend

--- backend/service/product.service.js
+++ backend/service/product.service.js
@@ -2,18 +2,13 @@ import pool from '../libs/postgres.js';
 
 export default class ProductsService {
 
-    constructor(){
-        this.products = [];
-
-    }
-
     async find(){
         try {
             const result = await pool.query("SELECT codigo, nombre, presentacion, descripcion FROM productos");
             return ({ message: "Productos", rows: result.rows });
         } catch (err) {
             console.error(err);
-            return ({ message: err || "Blank message" });
+            return ({ message: "Error message" });
         }
     }
 

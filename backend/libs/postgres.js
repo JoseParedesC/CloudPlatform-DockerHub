b8fab15ce94f671999ca2a2883f6321edd893763
Jose Dionisio Paredes Caballero <jdparedesc@siesa.com>
2025-10-31 10:44:53 -0500

    da

--- backend/libs/postgres.js
+++ backend/libs/postgres.js
@@ -1,13 +1,21 @@
 import { Pool } from 'pg';
-import dbConfig from '../libs/configdb.js'
+// import dbConfig from '../libs/configdb.js'
 
+const {
+    DB_HOST,
+    DB_USER,
+    DB_CATALOG,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_DIALECT
+} = process.env;
 
 const pool = new Pool({
-    host: dbConfig.DB_HOST, // nombre del servicio en docker-compose // ID_proyecto:REGION:INSTANCIA
-    database: dbConfig.DB_CATALOG,
-    user: dbConfig.DB_USER,
-    password: dbConfig.DB_PASSWORD,
-    port: dbConfig.DB_PORT
+    host: DB_HOST, // nombre del servicio en docker-compose // ID_proyecto:REGION:INSTANCIA
+    database: DB_CATALOG,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT
 });
 
 // module.exports = pool;
